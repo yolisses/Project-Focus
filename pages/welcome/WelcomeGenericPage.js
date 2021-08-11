bb2370060aa7940fc44020173e7e9697e7c9fe96
Ulisses <m.stunik@gmail.com>
2021-08-11 11:23:41 -0300

    change the welcome images heigth to relative

--- pages/welcome/WelcomeGenericPage.js
+++ pages/welcome/WelcomeGenericPage.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { Dimensions } from 'react-native';
+
+const width = Dimensions.get('window').width;
+
 export function WelcomeGenericPage(props) {
 	const Svg = props.svg;
 	return (
 		<View style={{ alignItems: 'center' }}>
-			<Svg height={140} />
+			<Svg
+				height={width / 3}
+				// height={140}
+			/>
 			<Text style={styles.text}>{props.children}</Text>
 			{props.button}
 		</View>
